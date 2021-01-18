e4ff818d991c2e4b0a15fe06696c07a154571bb9
한상진 <s202188@naver.com>
2021-01-18 21:27:25 +0900

    [fix] 파일 레퍼런스가 이상해서 빨간색으로 뜨던 오류 변경

--- BringChuChuba/Networking/Network.swift
+++ BringChuChuba/Networking/Network.swift
@@ -9,12 +9,21 @@ import Moya
 import RxSwift
 
 final class Network {
+    // MARK: - Errors
+    private enum RequestError: Error {
+        case FamilyExist
+        case Unknown
+    }
+
+    // MARK: - Initializers
     static let shared = Network()
     private init() {}
 
+    // MARK: - Properties
 //    private let provider = MoyaProvider<Router>(plugins: [NetworkLoggerPlugin()]) // for logging
     private let provider = MoyaProvider<Router>()
 
+    // MARK: - API Calls
     func request<T>(with httpRequest: Router, for returnType: T.Type) -> Single<T> where T: Decodable {
         return provider.rx.request(httpRequest)
             .debug()
@@ -34,70 +43,5 @@ final class Network {
                 throw err
             }
     }
-
-    // MARK: - 혹시 몰라서 남겨놓음
-
-//    func getMember() -> Single<Member> {
-//        return provider.rx.request(.getMember)
-//            .debug()
-//            .filterSuccessfulStatusCodes() // validate 200 - 299
-//            .map(Member.self) // decode
-//            .catchError { error in
-//                throw error
-//            }
-//    }
-//
-//    func getFamily(familyUid: Int) -> Single<Family> {
-//        return provider.rx.request(.getFamily(familyUid: familyUid))
-//            .debug()
-//            .filterSuccessfulStatusCodes()
-//            .map(Family.self)
-//    }
-//
-//    func getMissions(familyId: String) -> Single<[Mission]> {
-//        return provider.rx.request(.getMissions(familyId: familyId))
-//            .debug()
-//            .filterSuccessfulStatusCodes()
-//            .map([Mission].self)
-//    }
-//
-//    func createFamily(familyName: String) -> Single<Family> {
-//        return provider.rx.request(.createFamily(familyName: familyName))
-//            .debug()
-//            .filterSuccessfulStatusCodes()
-//            .map(Family.self)
-//    }
-//
-//    func createMission(missionDetails: MissionDetails) -> Single<Void> {
-//        return provider.rx.request(.createMission(missionDetails: missionDetails))
-//            .debug()
-//            .filterSuccessfulStatusCodes()
-//            .map { _ in }
-//    }
-//
-//    func joinFamily(familyId: String) -> Single<Family> {
-//        return provider.rx.request(.joinFamily(familyId: familyId))
-//            .debug()
-//            .filterSuccessfulStatusCodes()
-//            .map(Family.self)
-//    }
-//
-//    func contractMission(missionUid: Int) -> Single<Mission> {
-//        return provider.rx.request(.contractMission(missionUid: missionUid))
-//            .debug()
-//            .filterSuccessfulStatusCodes()
-//            .map(Mission.self)
-//    }
-//
-//    func completeMission(missionUid: Int) -> Single<Mission> {
-//        return provider.rx.request(.completeMission(missionUid: missionUid))
-//            .debug()
-//            .filterSuccessfulStatusCodes()
-//            .map(Mission.self)
-//    }
 }
 
-private enum RequestError {
-    case FamilyExist
-    case Unknown
-}
